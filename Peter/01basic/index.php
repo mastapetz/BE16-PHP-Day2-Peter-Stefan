25fca2e18e3f95789d001f83010ee04dc19a1f9c
Peter Aigner <mastapetz@icloud.com>
2022-07-12 10:18:36 +0200

    basic ex1

--- Peter/01basic/index.php
+++ Peter/01basic/index.php
@@ -7,6 +7,24 @@
     <title>Basic</title>
 </head>
 <body>
-    
+
+    <p>Excercise 1</p>
+    <form action="index.php" method = "POST">
+        Name: <input type="text" name="name">
+        Surname: <input type="text" name="surname">
+        <input type="submit" name="submit">
+    </form>
+    <?php
+        if( isset($_POST['submit'])){
+            if($_POST['name'] && $_POST['surname']){
+                echo "Hello {$_POST['name']} {$_POST['surname']}";
+            }
+            elseif(!$_POST['name'] && $_POST['surname']){
+                echo "Please insert first name";
+            }elseif(($_POST['name'] && !$_POST['surname'])){
+                echo "Please insert last name";
+            }
+        }
+    ?>
 </body>
 </html>
